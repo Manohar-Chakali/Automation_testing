e036c279ef73f0ee26309f9eadd61f311aaf3243
Manohar-Chakali <manoharchakali08@gmail.com>
2025-03-01 02:00:46 +0530

    Updated run.bat file

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
 @REM pytest -s -v --html=reports/Automation_report2.html .\testCases
-@REM pytest -s -v --alluredir="C:\Users\Lenovo\PycharmProjects\E-commersPorject\reports" ".\testCases\Account Registration Test Cases\test_001_Accountregistrationpage.py"
 
 pytest -s -v --alluredir="C:\Users\Lenovo\PycharmProjects\E-commersPorject\reports" .\testCases
