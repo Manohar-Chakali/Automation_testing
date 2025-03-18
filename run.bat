836b260e7ad83582859970e100db02aad8484bf0
Manohar-Chakali <manoharchakali08@gmail.com>
2025-03-18 23:29:01 +0530

    Altering reports

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 
 @REM pytest -s -v --alluredir="C:\Users\Lenovo\PycharmProjects\E-commersPorject\reports" .\testCases
 
-pytest -s -v --alluredir="reports" --junitxml="reports/junit_report.xml" ".\testCases\Logout Test Cases" || exit 0
+pytest -s -v --alluredir="C:/Users/Lenovo/PycharmProjects/E-commersPorject/reports" --junitxml="C:/Users/Lenovo/PycharmProjects/E-commersPorject/reports/junit_report.xml" ".\testCases\Logout Test Cases" || exit 0
